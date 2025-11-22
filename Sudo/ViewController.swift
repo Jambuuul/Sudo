61d8790e113c0622602df4867b5c77b50a3ec2e7
jam <jamalabdulov@mail.ru>
2025-11-22 19:47:20 +0300

    Initial Commit

---
+++ Sudo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudo
+//
+//  Created by Jam on 22.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
